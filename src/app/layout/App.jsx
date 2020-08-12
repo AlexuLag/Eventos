7bf7aebad244a59e355d5d5a746e66f10840ba46
John alexander ubaque laguna <alexander80143@gmail.com>
2020-08-11 20:48:18 -0500

    se eagrega formulario y lista

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
+import { Container } from "semantic-ui-react";
+
 
 function App() {
+  const [formOpen,setFormOpen] = useState(false);
   return (
-    <div className="App">
-    
-    </div>
+    <>
+     <NavBar setFormOpen={setFormOpen}/>
+     <Container className='main'>
+     <EventDashboard formOpen={formOpen}  setFormOpen={setFormOpen}/>
+     </Container>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../app/layout/styles.css';
+import 'semantic-ui-css/semantic.min.css';
+import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
@@ -13,7 +14,7 @@ function render(){
 
 
 if(module.hot){
-  module.hot.accept('./App.js', function(){
+  module.hot.accept('./app/layout/App', function(){
 
   })
 }
@@ -24,4 +25,4 @@ render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
